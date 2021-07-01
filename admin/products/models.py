9376681644b41d593978ff4c66575a5ddbb60de9
Pierre-Olivier Boisvert <pierre-olivierboisvert@Pierre-Oliviers-Mac-mini.local>
2021-06-30 23:48:25 -0400

    DJango migration Models

---
+++ admin/products/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    image = models.CharField(max_length=200)
+    likes = models.PositiveIntegerField(default=0)
+
+class User(models.Model):
+    pass
