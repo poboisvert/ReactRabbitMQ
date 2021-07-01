b89f7229db7dde92cefbade6a563900d300f7c81
Pierre-Olivier Boisvert <pierre-olivierboisvert@Pierre-Oliviers-Mac-mini.local>
2021-07-01 00:29:32 -0400

    Route post - api/products

--- admin/products/views.py
+++ admin/products/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
-
+# from django.shortcuts import render
 # Create your views here.
+from .serializers import ProductSerializer
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+
+from .models import Product
+from .serializers import ProductSerializer
+
+# Views
+class ProductViewSet(viewsets.ViewSet):
+    def list(self, request):
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+
+    def create(self, request):
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def retrieve(self, request, pk=None): # pk = primary key
+        product = Product.objects.get(id=pk)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+
+    def destroy(self, request, pk=None):
+        pass
